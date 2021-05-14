465626c4e39d838c617f034129283d956ca1dfc4
HenryQuillin <henryquillin@gmail.com>
2021-05-14 11:20:32 -0500

    init

---
+++ pathfinding-algorithm-visualizer.py
@@ -0,0 +1,11 @@
+import pygame
+import math
+from queue import PriorityQueue
+
+width = 800
+
+
+
+WIN = pygame.display.set_mode((width, width))
+pygame.display.set_caption("Pathfinding Algorithm Visualizer")
+
