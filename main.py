251df30f38f57f58004eed69808ef78ef6a6f1fd
Henry Quillin <henryquillin@gmail.com>
2021-09-06 21:18:00 -0500

    added funcationality to remove walls and start/end nodes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-# Henry Quillin 
-# Pathfinding Algorithm Visualizer 
-# 5 - 19 - 2021 
+# Henry Quillin
+# Pathfinding Algorithm Visualizer
+# 5 - 19 - 2021
 import pygame
 
 
 width = 800
 height = 600
-# Initialize colors that will represent node states  
+# Initialize colors that will represent node states
 CLOSED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (64, 206, 227)
@@ -20,13 +20,12 @@ END = (64, 224, 208)
 
 rows = 50
 
-# Initialize PyGame environmet 
+# Initialize PyGame environmet
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Pathfinding Algorithm Visualizer")
 
 
-
-class Node:  
+class Node:
     def __init__(self, row, col, node_width):
         self.row = row
         self.col = col
@@ -36,12 +35,12 @@ class Node:
         self.neighbors = []
         self.width = node_width
 
-    def draw(self):  # draw node method 
+    def draw(self):  # draw node method
         pygame.draw.rect(
             win, self.color, (self.x, self.y, self.width, self.width))
 
 
-def make_grid(): # Creates an array the size of the grid that contains node objects 
+def make_grid():  # Creates an array the size of the grid that contains node objects
     grid = []
     gap = width // rows
     for i in range(rows):
@@ -53,7 +52,7 @@ def make_grid(): # Creates an array the size of the grid that contains node obje
     return grid
 
 
-def draw_grid():  # Draws grid lines 
+def draw_grid():  # Draws grid lines
     gap = width // rows
     for i in range(rows):
         pygame.draw.line(win, GREY, (0, i * gap), (width, i * gap))
@@ -61,7 +60,7 @@ def draw_grid():  # Draws grid lines
             pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))
 
 
-def draw(grid_arr): # Draws nodes on the grid 
+def draw(grid_arr):  # Draws nodes on the grid
     win.fill(EMPTY)
 
     for row in grid_arr:
@@ -72,7 +71,7 @@ def draw(grid_arr): # Draws nodes on the grid
     pygame.display.update()
 
 
-def get_clicked_pos(pos): # Gets the current position of the mouse 
+def get_clicked_pos(pos):  # Gets the current position of the mouse
     gap = width // rows
     y = pos[0]
     x = pos[1]
@@ -88,30 +87,38 @@ def main():  # Main loop function
     end = None
     run = True
     started = False
-    while run: 
-        draw(grid) # passes the grid array into the draw function 
-        for event in pygame.event.get(): # check if the user exits the application 
+    while run:
+        draw(grid)  # passes the grid array into the draw function
+        for event in pygame.event.get():  # check if the user exits the application
             if event.type == pygame.QUIT:
                 run = False
 
-            if started: # When the simulation is started, don't allow the user to draw walls 
+            if started:  # When the simulation is started, don't allow the user to draw walls
                 continue
 
-            if pygame.mouse.get_pressed()[0]:  # If the user left clicks 
-                pos = pygame.mouse.get_pos().   # get the postion of the mouse 
+            if pygame.mouse.get_pressed()[0]:  # If the user left clicks
+                pos = pygame.mouse.get_pos()   # get the postion of the mouse
                 row, col = get_clicked_pos(pos)
-                node = grid[row][col]                         
-                if not start and node != end   # if the start and end node have not been placed
-                    start = node                 
+                node = grid[row][col]
+                if not start and node != end:   # if the start and end node have not been placed
+                    start = node
                     start.color = START       # place start node
 
                 elif not end and node != start:  # if the end node has not been placed
                     end = node
-                    end.color = END         # place end node 
+                    end.color = END         # place end node
 
                 elif node != end and node != start:   # if both the start and end node have been placed
                     node.color = WALL        # place a wall node
-                
+            if pygame.mouse.get_pressed()[2]:  # If the user right clicks
+                pos = pygame.mouse.get_pos()   # get the postion of the mouse
+                row, col = get_clicked_pos(pos)
+                node = grid[row][col]
+                node.color = EMPTY
+                if node == start:
+                    node = None
+                elif node == end:
+                    end = None
 
     pygame.quit()
 
