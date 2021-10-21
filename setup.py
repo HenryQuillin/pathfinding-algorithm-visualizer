837077824b8c69f90422939fdd4acfb1019b037c
Henry Quillin <henryquillin@gmail.com>
2021-10-21 10:24:15 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='pathfinding-algorithm-visualizer',
+    version='',
+    packages=[''],
+    url='https://github.com/HenryQuillin/pathfinding-algorithm-visualizer',
+    license='MIT license',
+    author='Henry Quillin',
+    author_email='henryquillin@gmail.com',
+    description='',
+    install_requires=[
+        "pygame~=2.0.1",
+        "pygame_gui~=0.5.7",
+        "setuptools~=57.0.0"
+    ]
+
+)
